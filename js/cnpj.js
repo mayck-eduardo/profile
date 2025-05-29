e5f8c52023c4a4cd8a4db86927debabfc79f3afe
Mayck Eduardo <mayck_22.03@hotmail.com>
2025-05-29 14:10:23 -0300

    ajusta a formatação do código, melhora a estrutura do HTML e adiciona comentários para clareza

--- js/cnpj.js
+++ js/cnpj.js
@@ -1,28 +1,28 @@
-    function buscaCEP() {
+function buscaCEP() {
 
-        let cnpj = document.getElementById('cep').value;
+    let cnpj = document.getElementById('cep').value;
 
-       
-        // Define the API URL
-        const apiUrl = `https://brasilapi.com.br/api/cnpj/v1/${cnpj}`;
-        console.log(apiUrl);
 
-        // Make a GET request
-        fetch(apiUrl)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-                document.getElementById('resultado').innerHTML = ` <a href="${apiUrl}" target="_blank">Clique aqui</a>`;
-               
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                document.getElementById('cep').innerText = 'Erro ao obter dados';
-            });
+    // Define the API URL
+    const apiUrl = `https://brasilapi.com.br/api/cnpj/v1/${cnpj}`;
+    console.log(apiUrl);
 
-    }
+    // Make a GET request
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            document.getElementById('resultado').innerHTML = ` <a href="${apiUrl}" target="_blank">Clique aqui</a>`;
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('cep').innerText = 'Erro ao obter dados';
+        });
+
+}
